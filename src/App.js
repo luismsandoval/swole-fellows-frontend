d6ef366c2f0605c2744da3a24d4790a6d266d86a
Luis Sandoval <luismsandoval96@gmail.com>
2022-06-22 21:08:20 -0700

    added handler function to lift state

--- src/App.js
+++ src/App.js
@@ -1,55 +1,60 @@
-import React from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
-import Header from './Header';
-import Footer from './Footer';
-import Dashboard from './Dashboard';
-import Recipes from './Recipes';
-import About from './About';
-import ProfilePage from './Profile';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+import Dashboard from "./Dashboard";
+import Recipes from "./Recipes";
+import About from "./About";
+import ProfilePage from "./Profile";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentWeight: "",
+      targetCalories: "",
+    };
+  }
+
+  getStats = (weight, calories) => {
+    this.setState({
+      currentWeight: weight,
+      targetCalories: calories,
+    });
+  };
 
   render() {
     return (
       <>
-
         <Router>
           <Header />
-          <Routes >
+          <Routes>
             <Route
               id="route"
-              exact path="/"
-              element={<Dashboard />}
-            >
-            </Route>
+              exact
+              path="/"
+              element={
+                <Dashboard
+                  currentWeight={this.state.currentWeight}
+                  targetCalories={this.state.targetCalories}
+                />
+              }
+            ></Route>
+            <Route exact path="/recipes" element={<Recipes />}></Route>
             <Route
-              exact path="/recipes"
-              element={<Recipes />}
-            >
-            </Route>
-            <Route
-              exact path='/profile'
-              element={<ProfilePage />}
-            >
-            </Route>
-            <Route
-              exact path='/about'
-              element={<About />}
-            >
-            </Route>
+              exact
+              path="/profile"
+              element={<ProfilePage getStats={this.getStats} />}
+            ></Route>
+            <Route exact path="/about" element={<About />}></Route>
           </Routes>
           <Footer />
         </Router>
       </>
-    )
+    );
   }
 }
-
 
 export default App;
